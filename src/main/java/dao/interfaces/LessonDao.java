1c9d4fdcd16a5b43ae89cc92c59930a52d375855
Denys <denis.gorban@protonmail.ch>
2021-04-19 21:48:53 +0300

    Added methods to LessonDao

--- src/main/java/dao/interfaces/LessonDao.java
+++ src/main/java/dao/interfaces/LessonDao.java
@@ -9,4 +9,12 @@ public interface LessonDao extends Dao<Lesson> {
     List<Lesson> findScheduledLessonsForTime(int timeId) throws DaoException;
 
     void assignLessonToTime(int lessonId, int timeId) throws DaoException;
+
+    List<Lesson> findLessonsWithCourse(int courseId) throws DaoException;
+
+    List<Lesson> findLessonsWithProfessor(int professorId) throws DaoException;
+
+    List<Lesson> findLessonsWithGroup(int groupId) throws DaoException;
+
+    List<Lesson> findLessonsWithAuditory(int auditoryId) throws DaoException;
 }
