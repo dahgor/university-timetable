cd435dce520055b6447fc4199557a27c64fd3ae8
Denys <denis.gorban@protonmail.ch>
2021-04-19 21:29:05 +0300

    Added initialization to getQuery method

--- src/main/java/dao/DaoProperties.java
+++ src/main/java/dao/DaoProperties.java
@@ -9,20 +9,23 @@ public class DaoProperties {
     private Properties properties;
     private FileInputStream file;
 
-    public DaoProperties(FileInputStream file){
+    public DaoProperties(FileInputStream file) {
         this.file = file;
     }
 
-    public void initialize() throws DaoException {
+    public String getQuery(String key) throws DaoException {
+        if (properties == null) {
+            initialize();
+        }
+        return properties.getProperty(key);
+    }
+
+    private void initialize() throws DaoException {
         properties = new Properties();
         try {
             properties.load(file);
         } catch (IOException e) {
             throw new DaoException("Cannot load properties file");
         }
-    }
-
-    public String getQuery(String key){
-        return properties.getProperty(key);
     }
 }
