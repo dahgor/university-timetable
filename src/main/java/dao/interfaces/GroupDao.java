bddae9ec4df3b40ef0ab01b8f59569fb9a8f6712
Denys <denis.gorban@protonmail.ch>
2021-04-27 22:48:43 +0300

    Added new method to GroupDao interface

--- src/main/java/dao/interfaces/GroupDao.java
+++ src/main/java/dao/interfaces/GroupDao.java
@@ -3,6 +3,8 @@ package dao.interfaces;
 import dao.DaoException;
 import dao.entities.Group;
 
+import java.util.List;
+
 public interface GroupDao extends Dao<Group> {
 
     void changeName(int groupId, String newName) throws DaoException;
@@ -10,4 +12,6 @@ public interface GroupDao extends Dao<Group> {
     void assignGroupToCourse(int groupId, int courseId) throws DaoException;
 
     void deleteGroupFromCourse(int groupId, int courseId) throws DaoException;
+
+    List<Group> findByCourse(int courseId) throws DaoException;
 }
