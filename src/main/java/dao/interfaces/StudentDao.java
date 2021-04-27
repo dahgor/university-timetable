dacff03ea4d60b5f4f9276bfa3c20f24402fc319
Denys <denis.gorban@protonmail.ch>
2021-04-27 18:54:01 +0300

    Added functionality to StudentDao interface

--- src/main/java/dao/interfaces/StudentDao.java
+++ src/main/java/dao/interfaces/StudentDao.java
@@ -6,5 +6,12 @@ import dao.entities.Student;
 import java.util.List;
 
 public interface StudentDao extends Dao<Student> {
+
+    void changeGroup(int studentId, int groupId) throws DaoException;
+
+    void changeFirstName(int studentId, String newFirstName) throws DaoException;
+
+    void changeLastName(int studentId, String newLastName) throws DaoException;
+
     List<Student> findStudentsInGroup(int groupId) throws DaoException;
 }
