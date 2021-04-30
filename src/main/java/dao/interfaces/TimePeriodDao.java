aeebf2c0912445494743bf03ef9b798d0df04627
Denys <denis.gorban@protonmail.ch>
2021-04-30 18:13:54 +0300

    Extended Dao with TimePeriodDao

--- src/main/java/dao/interfaces/TimePeriodDao.java
+++ src/main/java/dao/interfaces/TimePeriodDao.java
@@ -1,10 +1,11 @@
 package dao.interfaces;
 
 import dao.DaoException;
+import dao.entities.TimePeriod;
 
 import java.sql.Timestamp;
 
-public interface TimePeriodDao {
+public interface TimePeriodDao extends Dao<TimePeriod> {
 
     void changeStartHour(int timePeriodId, Timestamp newTime) throws DaoException;
 
